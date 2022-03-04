d69373c90000fda25ec90ad6ebba5fd859dc23b7
Arthur Liefhooghe <aliefhooghe@gmail.com>
2022-03-04 22:31:25 +0100

    Add gpu textures + cuda exceptions

---
+++ gpu_texture.cu
@@ -0,0 +1,90 @@
+
+#include "cuda_exception.cuh"
+#include "gpu_texture.cuh"
+
+namespace Xrender
+{
+
+    registered_texture::mapped_surface::mapped_surface(registered_texture& texture)
+    :   _texture{&texture}
+    {
+        cudaArray_t array;
+
+        CUDA_CHECK(cudaGraphicsMapResources(1, &(_texture->_graphic_resource), nullptr));
+        CUDA_CHECK(cudaGraphicsSubResourceGetMappedArray(&array, _texture->_graphic_resource, 0, 0));
+
+        cudaResourceDesc resource_desc;
+        resource_desc.resType = cudaResourceTypeArray;
+        resource_desc.res.array.array = array;
+
+        CUDA_CHECK(cudaCreateSurfaceObject(&_surface_object, &resource_desc));
+    }
+
+    registered_texture::mapped_surface::mapped_surface(registered_texture::mapped_surface&& other) noexcept
+    :   _texture{other._texture},
+        _surface_object{other._surface_object}
+    {
+        other._texture = nullptr;
+    }
+
+    __host__ registered_texture::mapped_surface::~mapped_surface() noexcept
+    {
+        if (_texture != nullptr) {
+            CUDA_WARNING(cudaDestroySurfaceObject(_surface_object));
+            CUDA_WARNING(cudaGraphicsUnmapResources(1, &(_texture->_graphic_resource), NULL));
+        }
+    }
+
+    __host__ cudaSurfaceObject_t registered_texture::mapped_surface::surface() const noexcept
+    {
+        return _surface_object;
+    }
+
+    __host__ registered_texture::registered_texture(GLuint texture_id, unsigned int width, unsigned int height)
+        : _width{width}, _height{height}, _gl_id{texture_id}
+    {
+        CUDA_CHECK(cudaGraphicsGLRegisterImage(
+            &_graphic_resource, _gl_id, GL_TEXTURE_2D, cudaGraphicsRegisterFlagsNone));
+    }
+
+    __host__ registered_texture::~registered_texture() noexcept
+    {
+        CUDA_WARNING(cudaGraphicsUnregisterResource(_graphic_resource));
+    }
+
+    __host__ unsigned int registered_texture::get_width() const noexcept
+    {
+        return _width;
+    }
+
+    __host__ unsigned int registered_texture::get_height() const noexcept
+    {
+        return _height;
+    }
+
+    __host__ GLuint registered_texture::get_gl_texture_id() const noexcept
+    {
+        return _gl_id;
+    }
+
+    __host__ registered_texture::mapped_surface registered_texture::get_mapped_surface()
+    {
+        return mapped_surface{*this};
+    }
+
+    __host__ std::vector<float4> registered_texture::retrieve_texture()
+    {
+        std::vector<float4> host_texture{_width * _height};
+        cudaArray_t array;
+
+        CUDA_CHECK(cudaGraphicsMapResources(1, &_graphic_resource, nullptr));
+        CUDA_CHECK(cudaGraphicsSubResourceGetMappedArray(&array, _graphic_resource, 0, 0));
+
+        CUDA_CHECK(cudaMemcpy2DFromArray(
+            host_texture.data(), _width * sizeof(float4), array,
+            0, 0, _width * sizeof(float4), _height, cudaMemcpyDeviceToHost));
+
+        CUDA_WARNING(cudaGraphicsUnmapResources(1, &_graphic_resource, NULL));
+        return host_texture;
+    }
+}
---
+++ gpu_texture.cuh
@@ -0,0 +1,67 @@
+#ifndef GPU_TEXTURE_H_
+#define GPU_TEXTURE_H_
+
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <vector>
+
+#include <cudaGL.h>
+#include <cuda_gl_interop.h>
+
+namespace Xrender
+{
+
+    class registered_texture
+    {
+
+    public:
+        class mapped_surface
+        {
+            friend class registered_texture;
+
+        public:
+            mapped_surface(const mapped_surface &) = delete;
+            mapped_surface(mapped_surface &&) noexcept;
+            __host__ ~mapped_surface() noexcept;
+
+            __host__ cudaSurfaceObject_t surface() const noexcept;
+
+        private:
+            __host__ mapped_surface(registered_texture &);
+
+            registered_texture *_texture;
+            cudaSurfaceObject_t _surface_object;
+        };
+
+        friend class mapped_surface;
+
+        __host__ registered_texture(GLuint texture_id, unsigned int width, unsigned int height);
+        registered_texture(const registered_texture &) = delete;
+        registered_texture(registered_texture &&) noexcept = delete; // todo
+        __host__ ~registered_texture() noexcept;
+
+        __host__ unsigned int get_width() const noexcept;
+        __host__ unsigned int get_height() const noexcept;
+        __host__ GLuint get_gl_texture_id() const noexcept;
+
+        /**
+         * \brief Return a handle to a surface object mapped
+         * to the openGL texture.
+         */
+        __host__ mapped_surface get_mapped_surface();
+
+        /**
+         * \brief Download the tecture from the device
+         */
+        __host__ std::vector<float4> retrieve_texture();
+
+    private:
+        unsigned int _width;
+        unsigned int _height;
+        cudaGraphicsResource *_graphic_resource{nullptr};
+        GLuint _gl_id{0u};
+    };
+
+}
+
+#endif
